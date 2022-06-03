073b50a5bbde9830c7d679c0d62ffdebd985ab1a
hokhai846 <hokhai846@gmail.com>
2022-06-03 10:02:40 +0700

    dựng khung layout

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className={cx('wrapper')}>
+      <div className={cx('inner')}>
+        {/* logo */}
+        {/* search */}
+        {/* actions */}
+      </div>
+    </header>
+  );
 }
 
 export default Header;
