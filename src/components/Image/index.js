f9ace67a45aa3c406a06895355e3c7ed48c290c4
hokhai846 <hokhai846@gmail.com>
2022-06-03 13:21:13 +0700

    lây font, tạo ảnh dự phòng khi error, lấy icons

---
+++ src/components/Image/index.js
@@ -0,0 +1,35 @@
+import React, { forwardRef, useState } from 'react';
+import classNames from 'classnames';
+import images from '~/assets/images';
+import styles from './Image.module.scss';
+
+console.log(images.noImage);
+function Image(
+  {
+    src,
+    alt = 'tiltle image',
+    className,
+    fallback: customFallback = images.noImage,
+    ...props
+  },
+  ref,
+) {
+  const [fallback, setFallback] = useState('');
+
+  const handleError = () => {
+    setFallback(customFallback);
+  };
+
+  return (
+    <img
+      alt={alt}
+      ref={ref}
+      src={fallback || src}
+      onError={handleError}
+      className={classNames(styles.wrapper, className)}
+      {...props}
+    />
+  );
+}
+
+export default forwardRef(Image);
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
+import { ClearIcon, LoadingIcon, SearchIcon } from '~/components/Icons';
+import Image from '~/components/Image';
 
 const cx = classNames.bind(styles);
 
@@ -8,8 +11,28 @@ function Header() {
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
-        {/* logo */}
-        {/* search */}
+        <div className={cx('logo')}>
+          <img src={images.logo.default} alt="Tiktok" />
+        </div>
+        <div className={cx('search')}>
+          <input
+            type="text"
+            placeholder="Search accounts and videos"
+            spellCheck={false}
+          />
+          <button className={cx('clear')}>
+            <ClearIcon />
+          </button>
+          <LoadingIcon />
+          <button className={cx('search-btn')}>
+            <SearchIcon />
+          </button>
+        </div>
+        <Image
+          alt="noidasd"
+          src="https://picsum.phots/200/300"
+          fallback="https://picsum.photos/seed/picsum/200/300"
+        />
         {/* actions */}
       </div>
     </header>
