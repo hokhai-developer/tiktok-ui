a7d7902a76ac1f340da3da71123e9890e9427781
hokhai846 <hokhai846@gmail.com>
2022-06-04 17:30:16 +0700

    xử lý khi hover

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -9,17 +9,15 @@ import MenuItem from './MenuItem';
 const cx = classNames.bind(styles);
 
 function Menu({ children, items = [] }) {
-  console.log(items);
-
   const renderItems = (items) => {
     return items.map((item, index) => <MenuItem data={item} key={index} />);
   };
 
   return (
     <Tippy
-      visible={true}
       interactive
       placement="bottom-end"
+      delay={[0, 500]}
       render={(attrs) => (
         <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
           <PopperWrapper className={cx('menu-propper')}>
